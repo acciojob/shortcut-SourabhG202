ae56c2d7f62d58d1d3f5e0fe5d20355014ff9c53
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-03-24 18:00:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function shortcut(s1, s2) {
-  // your code here
-	if(str1 = "" || str2 = ""){
+  // your
+	if(!str1.trim() || !str2.trim()){
 		return "";
 	}
-	return str1[0]+str2[0];
+	return str1.trim()[0]+str2.trim()[0];
 }
 
-// Do not change the code below.
+/* Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
