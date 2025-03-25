9d9e162f779e03dcfaafc6ae5ae48726459ff8c0
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-03-25 10:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function shortcut(str1, str2) {
 	return str1.trim()[0]+str2.trim()[0];
 }
 
-/* Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Do not change the code below.
+const str1 = prompt("Enter str1:");
+const str2 = prompt("Enter str2:");
+alert(shortcut(str1, str2));
